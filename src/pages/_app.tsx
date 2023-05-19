1879657254df6c78c02bbbba3635a0ffcbc7ac4f
Mystic-Slice <vaashwath@gmail.com>
2023-05-19 22:51:58 +0530

    Fixed styles not loading on first load (MUI is actually setup properly only now)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,38 @@
-import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 import { UserProvider } from "~/contexts/UserProvider";
 
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../theme';
+import createEmotionCache from '../createEmotionCache';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (    
-    <UserProvider>
-      <Component {...pageProps}/>
-    </UserProvider>
-  )
-};
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <UserProvider>
+          <Component {...pageProps}/>
+        </UserProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
 
 export default MyApp;
