ea3827db5af44a9ed0a6e92d42982ba3e48c9f41
Mystic-Slice <vaashwath@gmail.com>
2023-05-19 22:34:39 +0530

    minor improvements

--- src/Components/LogOut.tsx
+++ src/Components/LogOut.tsx
@@ -1,15 +1,20 @@
 import { UserContext } from "~/contexts/UserProvider";
 import { Button } from "@mui/material";
 import { useContext } from "react";
+import { useRouter } from "next/router";
 
 export const LogOut = () => {
     const user = useContext(UserContext);
+    const router = useRouter();
     console.log(user);
 
     return (
         <div className="mb-10">
             <Button id="userLogOutButton" 
-                onClick={() => user.logout()} 
+                onClick={() => {
+                    user.logout();
+                    router.push('/login');
+                }} 
                 variant="outlined" 
                 sx={{mt:2}} 
                 className="w-full" 
